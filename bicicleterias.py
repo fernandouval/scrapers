bfdb17d22fd9afcb99c29e5b0bdab299f0bc96bb
Fernando Uval <fernandouval@gmail.com>
2014-04-21 19:40:09 -0300

    Primer bicicletería en 1122

---
+++ bicicleterias.py
@@ -0,0 +1,67 @@
+from datetime import date, datetime, time
+#from utils import download, load_data, save_data, parse_date
+import urllib2, sys
+import re, lxml.html, lxml.etree, StringIO, datetime, csv
+
+git = 'https://github.com/fernandouval/scrapers/blob/master/bicicleterias.py'
+
+def a1122():
+	gitdata = 'https://github.com/fernandouval/scrapers/tree/master/data/1122.csv'
+	page = 1
+	lastTitle = ''
+	while page:
+		url = 'http://www.1122.com.uy/rubro-zona/montevideo/bicicleterias/PRD1000147/Z01000&page='+str(page)
+		req = urllib2.Request(url)
+		response = urllib2.urlopen(req)
+		body = response.read()
+		doc = lxml.html.document_fromstring(body)
+		divs = doc.xpath('//div[@id="divCentroResultados"]/div')
+		print 'tamano: '+str(len(divs))
+		count = 0
+		page += 1
+		for div in divs:
+			#de la izquierda sacamos btitle0
+			anuncio = div.cssselect('a')[0]
+			title = anuncio.text_content()
+			print title
+			print count
+			anuncioUrl = anuncio.attrib['href']
+			if (count == 0):
+				if (lastTitle == title):
+					page = 0
+					break
+				else:
+					#Nos quedamos con el primero
+					lastTitle = title
+			count += 1
+			an_req = urllib2.Request(anuncioUrl)
+			an_response = urllib2.urlopen(an_req)
+			an_body = an_response.read()
+			an_doc = lxml.html.document_fromstring(an_body)
+			vcard = an_doc.cssselect('.vcard')[0]
+			addr = vcard.cssselect('.street-address')[0].text_content()+', '+ vcard.cssselect('.region')[0].text_content()
+			tel = vcard.cssselect('.tel')[0].text_content().replace("Tel.:", "").strip()
+			cel = vcard.cssselect('.cel')[0].text_content().replace("Cel.:", "").strip()
+			phone = tel+'-'+cel
+			web = vcard.cssselect('.webpage a')[0].attrib['href']
+			if (web.strip() == 'http://'):
+				web = ''
+			extra = vcard.cssselect('.hours')[0].text_content()+"\n"+vcard.cssselect('.additionaltext')[0].text_content()
+			lat = vcard.cssselect('.latitude')[0][0].attrib['title']
+			lon = vcard.cssselect('.longitude')[0][0].attrib['title']
+			geo = lat+","+lon
+			data = [
+				title.strip().encode('utf8', 'replace'),
+				addr.strip().encode('utf8', 'replace'),
+				geo, 
+				web, 
+				phone,
+				anuncioUrl,
+				git,
+				gitdata
+			];
+			with open('data/1122.csv', 'ab') as csvfile:
+				csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+				csvwriter.writerow(data)
+
+a1122()
